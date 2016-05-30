b31a648516e567b08d2d6b76988d6fc0cac2be58
Student <icyph0x@protonmail.com>
2016-05-30 11:34:21 +0530

    Add results page

---
+++ results.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+
+<html>
+	<!-- some imports-->
+	<link rel="stylesheet" type="text/css" href="css/material.min.css">
+	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="css/material.min.js"></script>
+	<!--link rel="stylesheet" href="css/material.min.css"-->
+	<!--script src="css/material.js"></script-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+	<title>Voting System</title>
+	<head>
+			<!-- Uses a transparent header that draws on top of the layout's background -->
+			<style>
+			.demo-layout-transparent {
+			background: url('test.png') center / cover;
+			}
+			.demo-layout-transparent .mdl-layout__header,
+			.demo-layout-transparent .mdl-layout__drawer-button {
+			/* This background is dark, so we set text to white. Use 87% black instead if
+				 your background is light. */
+			color: white;
+			}
+			</style>
+
+			<div class="demo-layout-transparent mdl-layout mdl-js-layout mdl-layout--fixed-tabs mdl-shadow--16dp" style="height: 200px">
+			<header class="mdl-layout__header mdl-layout__header--transparent mdl-shadow--16dp" >
+				<div class="mdl-layout__header-row">
+					<!-- Title -->
+					<span onclick="location.href='home.html'" style="cursor:pointer;" class="mdl-layout-title">NPS-HSR | Voting Software | 2016-2017</span>
+					<!--NOTE: We will use a different image and change the color of the text in the header accordingly -->
+					<!-- Add spacer, to align navigation to the right -->
+					<div class="mdl-layout-spacer"></div>
+					<!-- Navigation -->
+					<nav class="mdl-navigation">
+						<a class="mdl-navigation__link" href="">Student Vote</a>
+						<a class="mdl-navigation__link" href="">Teacher Vote</a>
+						<a class="mdl-navigation__link" href="">Add Details</a>
+						<a class="mdl-navigation__link" href="">View Results!</a>
+					</nav>
+				</div>
+			</header>
+			<main class="mdl-layout__content">
+			</main>
+			</div>
+		<body>
+		<div id="passform" style="margin-top: 164px;">
+			<table class="mdl-data-table mdl-shadow--16dp">
+				<tr class="mdl-data-table">
+					<td>Candidates</td>
+					<td>8A</td>
+					<td>8B</td>
+					<td>8C</td>
+					<td>9A</td>
+					<td>9B</td>
+					<td>9C</td>
+					<td>10A</td>
+					<td>10B</td>
+					<td>10C</td>
+					<td>11A</td>
+					<td>11B</td>
+					<td>11C</td>
+					<td>12A</td>
+					<td>12B</td>
+					<td>Teachers</td>
+					<td>Total</td>
+				</tr>
+				<tr>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+				</tr>
+			</table>
+		</div>
+		</body>
